983235c1a564c6919c0de5b43fec2da445d7e2ef
Marcin <m.ficek@infomex.pl>
2023-09-05 15:51:31 +0200

    + go lang implementation of BTInOrderSearch

--- src/day1GoLang/bt_search/bt_in_order.go
+++ src/day1GoLang/bt_search/bt_in_order.go
@@ -1,1 +1,19 @@
 package BTSearch
+
+func BTInOrder(head BinaryNode) []int {
+	var path []int
+
+	walkInOrder(&head, &path)
+
+	return path
+}
+
+func walkInOrder(curr *BinaryNode, path *[]int) {
+	if curr == nil {
+		return
+	}
+
+	walkInOrder(curr.left, path)
+	*path = append(*path, curr.value)
+	walkInOrder(curr.right, path)
+}
--- src/day1GoLang/bt_search/bt_in_order_test.go
+++ src/day1GoLang/bt_search/bt_in_order_test.go
@@ -1,1 +1,15 @@
 package BTSearch
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBTInOrder(t *testing.T) {
+	result := BTInOrder(Tree)
+	want := []int{5, 7, 10, 15, 20, 29, 30, 45, 50, 100}
+
+	if !reflect.DeepEqual(result, want) {
+		t.Errorf("want: %d, got %d", want, result)
+	}
+}
