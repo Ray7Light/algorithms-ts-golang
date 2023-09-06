728e9fb6c020e3635ef69b51551e05308ad81e26
Marcin <m.ficek@infomex.pl>
2023-09-06 15:24:19 +0200

    + go implementation of BTPostOrderSearch

--- src/day1GoLang/bt_search/bt_post_order.go
+++ src/day1GoLang/bt_search/bt_post_order.go
@@ -1,1 +1,19 @@
 package BTSearch
+
+func BTPostOrder(head BinaryNode) []int {
+	var path []int
+
+	walkPostOrder(&head, &path)
+
+	return path
+}
+
+func walkPostOrder(curr *BinaryNode, path *[]int) {
+	if curr == nil {
+		return
+	}
+
+	walkPostOrder(curr.left, path)
+	walkPostOrder(curr.right, path)
+	*path = append(*path, curr.value)
+}
--- src/day1GoLang/bt_search/bt_post_order_test.go
+++ src/day1GoLang/bt_search/bt_post_order_test.go
@@ -1,1 +1,15 @@
 package BTSearch
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBTPostOrder(t *testing.T) {
+	result := BTPostOrder(Tree)
+	want := []int{7, 5, 15, 10, 29, 45, 30, 100, 50, 20}
+
+	if !reflect.DeepEqual(result, want) {
+		t.Errorf("want: %d, got %d", want, result)
+	}
+}
